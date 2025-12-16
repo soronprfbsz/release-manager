ab95083e9585b61916c552a1a5e79e1896d8c4e0
soronprfbsz <160199757+soronprfbsz@users.noreply.github.com>
2025-12-16 18:27:49 +0900

    feat: cratedb 패치 생성 코드 수정

--- src/main/java/com/ts/rm/domain/patch/util/CrateDBScriptGenerator.java
+++ src/main/java/com/ts/rm/domain/patch/util/CrateDBScriptGenerator.java
@@ -61,7 +61,7 @@ public class CrateDBScriptGenerator extends AbstractScriptGenerator {
                 .replace("{{TO_VERSION}}", toVersion)
                 .replace("{{VERSION_COUNT}}", String.valueOf(versions.size()))
                 .replace("{{VERSION_METADATA}}", buildVersionMetadata(versions))
-                .replace("{{SQL_EXECUTION_COMMANDS}}", buildCrateDBSqlExecutionCommands(cratedbFiles));
+                .replace("{{SQL_EXECUTION_COMMANDS}}", buildCrateDBSqlExecutionCommands(cratedbFiles, versions));
 
         // 스크립트 저장
         saveScript(script, outputDirPath);
@@ -71,33 +71,43 @@ public class CrateDBScriptGenerator extends AbstractScriptGenerator {
      * CrateDB SQL 실행 명령어 생성
      *
      * <p>CrateDB는 VERSION_HISTORY와 관계없이 SQL 파일만 실행합니다.
+     * 버전 순서를 보장하기 위해 versions 리스트를 순회합니다.
      *
-     * @param files CrateDB SQL 파일 리스트
+     * @param files    CrateDB SQL 파일 리스트
+     * @param versions 버전 리스트 (순서 보장)
      * @return SQL 실행 명령어
      */
-    private String buildCrateDBSqlExecutionCommands(List<ReleaseFile> files) {
+    private String buildCrateDBSqlExecutionCommands(List<ReleaseFile> files, List<ReleaseVersion> versions) {
         // 버전별로 그룹화
         var filesByVersion = files.stream()
                 .collect(Collectors.groupingBy(f -> f.getReleaseVersion().getVersion()));
 
         StringBuilder commands = new StringBuilder();
 
-        // 버전별로 실행 명령 생성
-        filesByVersion.forEach((version, versionFiles) -> {
-            commands.append(String.format("log_step \"버전 %s 패치 적용 중...\"\n", version));
-            commands.append(String.format("cd \"%s\"\n", version));
+        // 버전 리스트 순서대로 실행 명령 생성 (순서 보장)
+        for (ReleaseVersion version : versions) {
+            String versionStr = version.getVersion();
+            List<ReleaseFile> versionFiles = filesByVersion.get(versionStr);
+
+            // 해당 버전에 CrateDB 파일이 없으면 스킵
+            if (versionFiles == null || versionFiles.isEmpty()) {
+                continue;
+            }
+
+            commands.append(String.format("log_step \"버전 %s 패치 적용 중...\"\n", versionStr));
+            commands.append(String.format("cd \"%s\"\n", versionStr));
 
             // 실행 순서대로 정렬된 파일 실행
             versionFiles.stream()
                     .sorted((a, b) -> Integer.compare(a.getExecutionOrder(), b.getExecutionOrder()))
                     .forEach(file -> {
                         commands.append(String.format("log_info \"실행: %s\"\n", file.getFileName()));
-                        commands.append(String.format("execute_sql_file \"%s\"\n", file.getFileName()));
+                        commands.append(String.format("execute_sql \"%s\"\n", file.getFileName()));
                     });
 
             commands.append("cd ..\n");
-            commands.append(String.format("log_success \"버전 %s 패치 완료!\"\n\n", version));
-        });
+            commands.append(String.format("log_success \"버전 %s 패치 완료!\"\n\n", versionStr));
+        }
 
         return commands.toString();
     }
